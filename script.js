230c4dba0813cd101e2f58d33a1612006f6868db
JoaoVictor2105 <joaovcdm21059@gmail.com>
2026-08-26 03:45:51 -0300

    feat: implementa lógica de números, operadores e botão de igual

---
+++ script.js
@@ -0,0 +1,42 @@
+const display = document.querySelector('#display');
+const previousoperand = document.querySelector('#previous-operand');
+const datanumber = document.querySelectorAll('[data-number]');
+const dataoperator = document.querySelectorAll('[data-operator]');
+const dataclear = document.querySelector('#clear');
+const datacalculate = document.querySelector('#equals');
+const datadecimal = document.querySelector('[data-action="decimal"]');
+
+let currentValue = '0';
+let previousValue = '';
+let operator = null;
+
+datanumber.forEach((button) => {
+     button.addEventListener('click', () => {
+        const digit = button.dataset.number;
+
+if (currentValue === '0') { currentValue = digit } else { currentValue += String(digit) }
+display.textContent = currentValue
+})});
+
+dataoperator.forEach((button) => {
+    button.addEventListener('click', () => {
+        const chosenOperator = button.dataset.operator
+
+previousValue = currentValue
+operator = chosenOperator
+currentValue = ''
+previousoperand.textContent = previousValue + ' '+ chosenOperator;
+})});
+
+let result = null;
+datacalculate.addEventListener('click', () => {
+    if (operator === '+') {result = parseInt(previousValue) + parseInt(currentValue)}
+    else if (operator === '-') {result = parseInt(previousValue) - parseInt(currentValue)}
+    else if (operator === '*') {result = parseInt(previousValue) * parseInt(currentValue)}
+    else if (operator === '/') {result = parseInt(previousValue) / parseInt(currentValue)}
+
+display.textContent = result
+previousoperand.textContent = ''
+currentValue = String(result)
+operator = null
+});
